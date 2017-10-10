90b3becd700c0ac737633d8220135fa453829d12
incarnation.p.lee <incarnation.p.lee@gmail.com>
2017-10-10 15:59:54 +0800

    Add goruntine and web server sample code.
    Signed-off-by: incarnation.p.lee <incarnation.p.lee@gmail.com>

--- manual/manual.go
+++ manual/manual.go
@@ -4,6 +4,7 @@ import "os"
 import "fmt"
 import "strings"
 import "bufio"
+import "io"
 import "io/ioutil"
 import "image"
 import "image/color"
@@ -11,6 +12,9 @@ import "image/gif"
 import "math"
 import "math/rand"
 import "net/http"
+import "time"
+import "log"
+import "sync"
 
 /* import ("fmt" "os") */
 
@@ -29,6 +33,10 @@ import "net/http"
  * 12. ReadFile return byte slice(something or array?), need to convert to string.
  * 13. Bufi.Scanner and ioutil.ReadFile are implemented by os.File read and write.
  * 14. []color.Color{} for slice and gif.GIF{} for structure, append internal function.
+ * 15. goruntine is one concurrence execution way, and channel is for the commnucation
+ *     between different goruntine. [go function] will create one new goruntime and then
+ *     execution the function. when go function try to access channel in one function
+ *     execution time, the channel need to handle the critical section itself.
  *
  */
 
@@ -44,8 +52,12 @@ func main() {
 	bufio_uniq()
 	bufio_file()
 	ioutil_readfile()
+
 	image_gif()
+
 	net_http_url_request()
+	net_http_url_go_runtine()
+	// net_web_server()
 }
 
 func os_args() {
@@ -104,8 +116,8 @@ func bufio_file() {
 	files := os.Args[0:]
 
 	for _, arg := range files {
-		fop, err := os.Open(arg) /* return opened file and err */
-		if err != nil {
+                /* return opened file and err */
+		if fop, err := os.Open(arg); err != nil {
 			fmt.Fprintf(os.Stderr, "Fail to open %s:%d\n", arg, err)
 		} else {
 			fmt.Fprintf(os.Stdout, "Opened file %s\n", arg)
@@ -189,5 +201,63 @@ func net_http_url_request() {
 	if is_print {
 		fmt.Printf("%s", b)
 	}
+}
+
+func fetch_url(url string, ch chan<- string ) {
+	start := time.Now()
+	resp, err := http.Get(url)
+
+	if err != nil {
+		ch <- fmt.Sprint(err) /* send to channel ch */
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s, %d\n", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+
+	ch <- fmt.Sprintf("%.4fs	%7d	%s", secs, nbytes, url);
+}
+
+func net_http_url_go_runtine() {
+	url_list := []string{"https://golang.org", "http://gopl.io",
+			"http://www.google.com", "http://www.bing.com"}
+	ch := make(chan string)
+
+	for _, url := range url_list {
+		go fetch_url(url, ch) /* start a go rountine */
+	}
+
+	for i := 0; i < len(url_list); i++ {
+		if is_print {
+			/*
+			 * Here main thread reach here, it will wait until
+			 * first goruntine finish touch channel string. And
+			 * then continue wait the second fast one to finish.
+			 */
+			fmt.Println(<-ch)
+		}
+	}
+}
+
+var mu sync.Mutex
+var count int = 0
+
+func web_server_handler (w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	fmt.Fprintf(w, "URL.Path = %q, count %d\n", r.URL.Path, count);
+	mu.Unlock()
+}
+
+func net_web_server() {
+	http.HandleFunc("/", web_server_handler)
+	log.Fatal(http.ListenAndServe("localhost:8888", nil))
 }
 
