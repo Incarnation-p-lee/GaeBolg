8fc9a238e32464e666c862cef87e91f6216d567e
incarnation.p.lee <incarnation.p.lee@gmail.com>
2017-10-10 08:18:32 +0800

    Add sample of image/gif and net.
    Signed-off-by: incarnation.p.lee <incarnation.p.lee@gmail.com>

--- manual/manual.go
+++ manual/manual.go
@@ -4,6 +4,14 @@ import "os"
 import "fmt"
 import "strings"
 import "bufio"
+import "io/ioutil"
+import "image"
+import "image/color"
+import "image/gif"
+import "math"
+import "math/rand"
+import "net/http"
+
 /* import ("fmt" "os") */
 
 /*
@@ -18,6 +26,10 @@ import "bufio"
  *  9. ++/-- is statement not expression. Illegal if ++/--i.
  * 10. := only available when initialize, or variable without var.
  * 11. Almost all build-in function variable with first letter upper, os.Args.
+ * 12. ReadFile return byte slice(something or array?), need to convert to string.
+ * 13. Bufi.Scanner and ioutil.ReadFile are implemented by os.File read and write.
+ * 14. []color.Color{} for slice and gif.GIF{} for structure, append internal function.
+ *
  */
 
 func main() {
@@ -29,6 +41,9 @@ func main() {
 
 	bufio_uniq()
 	bufio_file()
+	ioutil_readfile()
+	image_gif()
+	net_http_url_request()
 }
 
 func os_args() {
@@ -92,8 +107,83 @@ func bufio_file() {
 			fmt.Fprintf(os.Stderr, "Fail to open %s:%d\n", arg, err)
 		} else {
 			fmt.Fprintf(os.Stdout, "Opened file %s\n", arg)
+			/* bufio.NewScanner(fop) */
 			fop.Close()
 		}
 	}
+}
+
+func ioutil_readfile() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[0:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fail to open %s:%d\n", filename, err)
+		}
+
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+}
+
+func image_gif() {
+	var palette = []color.Color{color.White, color.Black}
+
+	const (
+		whiteIndex = 0 /* first color in palette */
+		blackIndex = 1
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2 * size + 1, 2 * size + 1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles * 2 * math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t * freq + phase)
+			img.SetColorIndex(size + int(x * size + 0.5),
+				size + int(y * size + 0.5),
+				blackIndex)
+		}
+
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	// Uncomment follow line will print gif to Stdout
+	// gif.EncodeAll(os.Stdout, &anim)
+}
+
+func net_http_url_request() {
+	url := "http://www.bing.com"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fetch %s:%d fail\n", url, err)
+		os.Exit(1)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Read fail %d\n", err)
+		os.Exit(1)
+	}
+
+	// Uncomment follow line will print http body to Stdout
+	// fmt.Printf("%s", b)
 }
 
