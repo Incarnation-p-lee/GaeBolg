f03c4b142ce2cf98dfb38ff6514ad1c4ca8ed421
Pan Li <incarnation.p.lee@gmail.com>
2017-10-21 21:12:15 +0800

    Refined problem 39.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- leetcode/problems/problem_39.go
+++ leetcode/problems/problem_39.go
@@ -5,46 +5,39 @@ import "sort"
 
 /* Problem 39. Combination Sum */
 func CombinationSum() {
-	candidates := []int{42,26,36,38,35,41,20,47,45,23,33,39,25,43,29,31,28,48,21,46,22,30,37,32,44,40}
-	target := 55
+	candidates := []int{2, 3, 6, 7}
+	target := 7
 
 	combinationSum(candidates, target)
 }
 
-func combinationDfs(in []int, i int, t int, buf []int, k int, out *[][]int) {
-	d := in[i]
+func combinationDfs(in []int, i int, t int, buf *[]int, out *[][]int) {
 
-	if i == len(in) || t < d {
+	if t < 0 {
 		return
-	} else if i > 0 && in[i] == in[i - 1] {
-		/* if sorted are same value, first one will cover next */
-		return
-	} else if t == d {
-		buf[k] = d
-		m := make([]int, k + 1)
-
-		copy(m, buf) /* copy instead of assignment */
+	} else if t == 0 {
+        m := make([]int, len(*buf))
+		copy(m, *buf)                     /* copy instead of assignment */
 		*out = append(*out, m)
-	} else if t > d {
-		t = t - d
-		buf[k] = d
+	} else {
+		for j := i; j < len(in); j++ {    /* from i to rest of slice */
+			d := in[j]
+			*buf = append(*buf, d)        /* push d */
 
-		for j := i; j < len(in); j++ { /* from i to rest of slice */
-			combinationDfs(in, j, t, buf, k + 1, out)
+			combinationDfs(in, j, t - d, buf, out)
+
+			*buf = (*buf)[:len(*buf) - 1] /* pop d */
 		}
 	}
 }
 
 func combinationSum(candidates []int, target int) [][]int {
+	var buf []int
 	var out [][]int
 
-	buf := make([]int, target)
 	sort.Ints(candidates)
 
-	for i, _ := range candidates {
-		/* for each element can be the start one */
-		combinationDfs(candidates, i, target, buf, 0, &out)
-	}
+	combinationDfs(candidates, 0, target, &buf, &out)
 
 	fmt.Println(out)
 
