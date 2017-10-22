067e2240b19236c615dc11a8c4312983814c5744
Pan Li <incarnation.p.lee@gmail.com>
2017-10-22 21:10:35 +0800

    Refine problem 46 for performance.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- leetcode/problems/problem_46.go
+++ leetcode/problems/problem_46.go
@@ -2,35 +2,32 @@ package problems
 
 import "fmt"
 
-func permuteDfs(nums []int, deep int, buf *[]int, out *[][]int, m *map[int]int) {
+func permuteDfs(nums []int, deep int, buf *[]int, out *[][]int, visit []bool) {
 	if deep == len(nums) {
 		data := make([]int, len(*buf))
 		copy(data, *buf)
 		*out = append(*out, data)
-		return
+	} else {
+		for i, v := range nums {
+			if !visit[i] {
+				*buf = append(*buf, v)
+				visit[i] = true
+
+				permuteDfs(nums, deep + 1, buf, out, visit)
+
+				*buf = (*buf)[:len(*buf) - 1]
+				visit[i] = false
+			}
+		} 
 	}
-
-	nMap := *m
-
-	for _, v := range nums {
-		if _, ok := nMap[v]; !ok {
-			*buf = append(*buf, v)
-			nMap[v] = v
-
-			permuteDfs(nums, deep + 1, buf, out, m)
-
-			*buf = (*buf)[:len(*buf) - 1]
-			delete(nMap, v)
-		}
-	} 
 }
 
 func permute(nums []int) [][]int {
-	var nMap map[int]int = make(map[int]int)
 	var buf []int
 	var out [][]int
+	var visit []bool = make([]bool, len(nums))
 
-	permuteDfs(nums, 0, &buf, &out, &nMap)
+	permuteDfs(nums, 0, &buf, &out, visit)
 
 	fmt.Println(out)
 
