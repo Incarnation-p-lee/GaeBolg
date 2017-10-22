f8ea8e0c89347fca84689bc3c9d75a8605206366
Pan Li <incarnation.p.lee@gmail.com>
2017-10-22 22:48:09 +0800

    Add problem 60.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -8,9 +8,12 @@ func main() {
 
 	pbms.LetterCombination()
 	pbms.GenerateParenthesis()
+
 	pbms.CombinationSum()
 	pbms.CombinationSum2()
+
 	pbms.Permute()
 	pbms.PermuteUnique()
+	pbms.GetPermutation()
 }
 
---
+++ leetcode/problems/problem_60.go
@@ -0,0 +1,60 @@
+package problems
+
+import "fmt"
+import "strconv"
+
+func getPermutationDfs(n []int, dp int, k *int, buf *[]int, visit []bool) bool {
+	if dp == len(n) {
+		(*k)--
+		if *k == 0 {
+			return true
+		}
+	} else {
+		for i, v := range n {
+			if !visit[i] {
+				*buf = append(*buf, v)
+				visit[i] = true
+
+				if ok := getPermutationDfs(n, dp + 1, k, buf, visit); ok {
+					return true
+				}
+
+				*buf = (*buf)[:len(*buf) - 1]
+				visit[i] = false 
+			}
+		}
+	}
+
+	return false
+}
+
+func getPermutation(n int, k int) string {
+	var s string
+	var buf []int
+	var nums []int = make([]int, n)
+	var visit []bool = make([]bool, n)
+
+	for i, _ := range nums {
+		nums[i] = i + 1
+	}
+
+	count := k
+	getPermutationDfs(nums, 0, &count, &buf, visit)
+
+	for _, v := range buf {
+		s += strconv.Itoa(v)
+	}
+
+	fmt.Println(s)
+
+	return s
+}
+
+
+func GetPermutation() {
+	n := 6
+	k := 23
+
+	getPermutation(n, k)
+}
+
