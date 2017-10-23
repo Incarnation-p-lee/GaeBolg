d5db78a705a0980ae855b7eca847119ebd11e458
incarnation.p.lee <incarnation.p.lee@gmail.com>
2017-10-23 13:45:27 +0800

    Add profile for leetcode.
    Signed-off-by: incarnation.p.lee <incarnation.p.lee@gmail.com>

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -1,9 +1,22 @@
 package main
 
 import "fmt"
+import "os"
+import "log"
+import "runtime/pprof"
+
 import pbms "./problems"
 
 func main() {
+	f, err := os.Create("cpu.prof")
+
+	if err != nil {
+		log.Printf("Failed to create cpu.prof\n")
+	} else {
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	fmt.Println("Hello, Leetcode!")
 
 	pbms.LetterCombination()
