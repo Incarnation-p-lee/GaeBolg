8fa43feef7dc01cd83b6c0c2ddf05bd377443627
Pan Li <incarnation.p.lee@gmail.com>
2017-10-23 22:39:40 +0800

    Add problem 77.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

---
+++ leetcode/problems/problem_77.go
@@ -0,0 +1,44 @@
+package problems
+
+import "fmt"
+
+func combineDfs(d []int, idx int, k int, buf *[]int, out *[][]int) {
+	if (len(*buf) == k) {
+		m := make([]int, k)
+		copy(m, *buf)
+		*out = append(*out, m)
+	} else {
+		for j := idx; j < len(d); j++ {
+			v := d[j]
+
+			*buf = append(*buf, v)
+			combineDfs(d, j + 1, k, buf, out)
+			*buf = (*buf)[:len(*buf) - 1]
+		}
+	}
+}
+
+func combine(n int, k int) [][]int {
+	var buf []int
+	var out [][]int
+	var data []int
+
+	for i := 0; i < n; i++ {
+		data = append(data, i + 1)
+	}
+
+	combineDfs(data, 0, k, &buf, &out)
+
+	fmt.Println(out)
+
+	return out
+}
+
+func Combine() {
+	n := 4
+	k := 2
+
+	// fmt.Printf("<77> ")
+	combine(n, k)
+}
+
